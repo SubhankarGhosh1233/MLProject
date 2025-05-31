a98b0682b7efef2f5cbe7db1df651299596f2892
Subhankar Ghosh <subhankar333ghosh@gmail.com>
2025-05-31 21:14:41 +0530

    DataTransformation & Util created & importing ingestion into data_ingestion from transformation & modified requirements.txt

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Use 12-hour format with AM/PM
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%I_%M_%S_%p')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s ] - %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] - %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
+    datefmt="%m/%d/%Y %I:%M:%S %p"  # 12-hour format with AM/PM
 )
